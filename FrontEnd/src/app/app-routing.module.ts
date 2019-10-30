b066801c900470a7e19b2dc25e8452465b7b1cdf
Oscar Bosigas <oscar.bosigas@uptc.edu.co>
2019-10-30 14:38:48 -0500

    index page

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InitComponent } from './components/init/init.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: InitComponent},
+  { path: 'contact', component: ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InitComponent } from './components/init/init.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InitComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
